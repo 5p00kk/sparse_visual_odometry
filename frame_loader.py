e544d9840443611c5fbe73ac44210dcb38e0fea8
szymon <szymon.kowalewski@gmail.com>
2020-05-08 20:36:28 +0200

    Remove trailing spaces

--- frame_loader.py
+++ frame_loader.py
@@ -4,12 +4,12 @@ class FrameLoader():
     def __init__(self, video_path):
         self.cap = cv2.VideoCapture(video_path)
         self.frame_num = 0
-    
+
     def get_frame(self):
         ret, frame = self.cap.read()
         if ret:
             self.frame_num += 1
-        
+
         return ret, frame
 
     def close(self):
