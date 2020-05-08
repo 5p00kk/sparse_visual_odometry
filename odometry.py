135e008f36edf212b1b149cb1dfb174f521caf51
szymon <szymon.kowalewski@gmail.com>
2020-05-08 21:16:32 +0200

    Add ORB extraction

--- odometry.py
+++ odometry.py
@@ -1,9 +1,21 @@
+import cv2 
+
 class FeatureExtractor():
-    def __init__(self):
-        pass
+    def __init__(self, type):
+        if type == "orb":
+            self.extractor = cv2.ORB_create()
+        else:
+            self.extractor = None
     
     def extract_features(self, frame):
-        pass
+        assert self.extractor is not None
+        
+        # Extract keypoints
+        kp = self.extractor.detect(frame, None)
+        # Compute descriptors
+        kp, des = self.extractor.compute(frame, kp)
+
+        return kp, des
 
     def filter_features(self, features):
         pass
