0404bc01187a0581bdf1d05a773ab20e147a8d5c
szymon <szymon.kowalewski@gmail.com>
2020-05-08 20:23:33 +0200

    Initial placeholder for odometry functions

---
+++ odometry.py
@@ -0,0 +1,16 @@
+class FeatureExtractor():
+    def __init__(self):
+        pass
+    
+    def extract_features(self, frame):
+        pass
+
+    def filter_features(self, features):
+        pass
+
+class Odometry():
+    def __init__(self):
+        pass
+
+    def do_odometry(self, kp_pairs):
+        pass
