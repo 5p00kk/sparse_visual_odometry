8d8b3c93c419a4f274d917e90f2fadae474d5b59
szymon <szymon.kowalewski@gmail.com>
2020-05-08 20:57:48 +0200

    Add visualization to main.py, add extra printouts

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from frame_loader import FrameLoader
 from odometry import FeatureExtractor
+from visualizator import Visualizator
 
 # Declarations
 frame_loader = FrameLoader("data/video.mp4")
 extractor = FeatureExtractor()
+visualizator = Visualizator()
 frame_read = True
 
-
 while frame_read:
+    # Load and display the frame
     frame_read, frame = frame_loader.get_frame()
-    print("Captured frame %d" % frame_loader.frame_num)
+    visualizator.show_image(frame)
+
+    # Printouts
+    print("Captured frame: %d" % frame_loader.frame_num)
+    print("Frame shape: %s" % (frame.shape,))
 
 frame_loader.close()
