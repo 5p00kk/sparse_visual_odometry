b3063ddaff5c511aaef8f391006119318390f472
szymon <szymon.kowalewski@gmail.com>
2020-05-08 21:17:03 +0200

    Add keypoint visualization

--- visualizator.py
+++ visualizator.py
@@ -7,3 +7,8 @@ class Visualizator():
     def show_image(self, image, wait=1):
         cv2.imshow("Image", image)
         cv2.waitKey(wait)
+
+    def show_keypoints(self, image, kp, wait=1):
+        kp_image = cv2.drawKeypoints(image, kp, None, color=(0,255,0), flags=0)
+        cv2.imshow("Keypoints", kp_image)
+        cv2.waitKey(wait)
